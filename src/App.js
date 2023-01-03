75f30f36ed4acc2912031e0f2cb4d5dd9355f63e
shiwank pathak <shiwankpathak12@gmail.com>
2023-01-03 16:14:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes, useNavigate,Router } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
+
+function App() {
+  //const navigate = useNavigate()
+  const [questions, setQuestions] = useState();
+  const [name, setName] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=5${category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    setQuestions(data.results);
+  };
+
+  return (
+    <BrowserRouter>
+      <div className="app" style={{  }}>
+        <Header />
+        <Routes>
+          
+          <Route path="/" exact element={<Home
+            name={name}
+            setName={setName}
+            fetchQuestions={fetchQuestions}
+          />}>
+
+          </Route>
+          <Route path="/quiz" element={<Quiz
+            name={name}
+            questions={questions}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+          />}>
+          </Route>
+          <Route path="/result" element={<Result name={name} score={score} />}>
+          </Route>
+        </Routes>
+        
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ by{" "}
+        <a
+          href="https://www.linkedin.com/in/shiwankpathak12/"
+          style={{ cursor: "pointer" }}
+        >
+          Shiwank Pathak
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
